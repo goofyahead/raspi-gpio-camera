e3d8bbd203718f1c60c79452a520a2a7bb1a88e8
Alejandro <goofyahead@gmail.com>
2014-01-06 01:55:11 +0100

    stepper

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var Gpio = require('onoff').Gpio,
-    button = new Gpio(17, 'in','falling');
+
+var Gpio = require('onoff').Gpio;
+var button = new Gpio(17, 'in','falling');
+var step = new Gpio(24,'out');
+var direction = new Gpio(23,'out');
+var iv;
+var stepStatus = 0;
 
 console.log('on waiting');
 
 button.watch(function(err, value) {
     if (err) exit();
     console.log('button pressed!');
+
+    iv = setInterval(function() {
+    	step.writeSync(stepStatus === 0 ? 1 : 0); // 1 = on, 0 = off :)
+    	stepStatus = stepStatus === 0 ? 1 : 0;
+	}, 200);
+	
 });
 
 function exit() {
