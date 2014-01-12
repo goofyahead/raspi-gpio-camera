f4346fa0aff41d68691dd36c8926ad9df37b00ae
Alejandro <goofyahead@gmail.com>
2014-01-12 19:08:19 +0100

    commands sepparated

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ button.watch(function(err, value) {
     	console.log('button pressed!');
 
     	
-	    var raspivid  = spawn('raspivid', ['-n']);
+	    var raspivid  = spawn('raspivid', ['-n', '-o', 'video1.h264', '-t', '20000']);
 
 		raspivid.on('close', function (code, signal) {
 		  console.log('child process terminated due to receipt of signal '+signal + ' and code ' + code);
