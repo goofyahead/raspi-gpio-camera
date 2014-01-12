391863d8f1d9352b0e7873c90fef8be6ab7a76aa
Alejandro <goofyahead@gmail.com>
2014-01-12 20:29:33 +0100

    testing msteps

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ button.watch(function(err, value) {
 
 	    iv = setInterval(function() {
 	    	//console.log('step ' + stepStatus);
-	    	if (count == 3801){
+	    	if (count == 3601){
 	    		clearInterval(iv);
 	    		count = 0;
 	    		//send sigterm or sigints
