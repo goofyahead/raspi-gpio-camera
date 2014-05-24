d0c011a781ccc4b54753212ab708a031c3a7b277
Alejandro <goofyahead@gmail.com>
2014-05-24 13:53:19 +0200

    rotate

---
+++ rotate.js
@@ -0,0 +1,26 @@
+
+var Gpio = require('onoff').Gpio;
+
+var step = new Gpio(24,'out');
+var iv;
+var stepStatus = 0;
+var timestamp = 0;
+var count = 0;
+var SPEED = 5;
+
+console.log('rotating');
+
+iv = setInterval(function() {
+	//console.log('step ' + stepStatus);
+	if (count == 2880){
+		clearInterval(iv);
+		count = 0;
+		//send sigterm or sigints
+		console.log('quiting after loop');
+	}
+
+	count++;
+	step.writeSync(stepStatus === 0 ? 1 : 0); // 1 = on, 0 = off :)
+	stepStatus = stepStatus === 0 ? 1 : 0;
+}, SPEED);
+    
